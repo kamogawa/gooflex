ee92ec0b76da96d4f063833f70a224c43ff1db12
BEUMGOO <captain_kbg@naver.com>
2019-07-27 22:31:23 +0900

    API Verbs

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ Movie Discovery App.
 
 ## API Verbs
 
-- [ ] Now playing (Movie)
-- [ ] Upcoming (Movie)
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
-- [ ] Airing Today (TV)
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV, Movie)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
--- src/api.js
+++ src/api.js
@@ -8,6 +8,34 @@ const api = axios.create({
   }
 });
 
-api.get("tv/popular");
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  movieDetail: (id) => api.get(`movie/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  searchMove: (term) => api.get("search/movie", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+}
 
-export default api;
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  showDetail: (id) => api.get(`tv/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  searchTv: (term) => api.get("search/tv", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+}
