4b796635c8eb2734b103cd6b40d6d50b4268f557
BEUMGOO <captain_kbg@naver.com>
2019-07-30 23:48:41 +0900

    Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component{
   state = {
@@ -10,8 +11,38 @@ export default class extends React.Component{
     loading: true
   };
 
+  async componentDidMount(){
+    try {
+      const { 
+        data: { results : nowPlaying } 
+      } = await movieApi.nowPlaying();
+      const { 
+        data: { results : upComing } 
+      } = await movieApi.upComing();
+      const { 
+        data: { results : popular } 
+      } = await movieApi.popular();
+      this.setState({
+        nowPlaying,
+        upComing,
+        popular
+      });
+    } catch (error) {
+      this.setState({
+        error: "Can't find movies infomation"
+      })
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
+  getNowPlauing
+
   render() {
     const { nowPlaying, upComing, popular, error, loading } = this.state
+    console.log(this.state);
     return (
     <HomePresenter 
       nowPlaying={nowPlaying} 
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    apikey: "d07205f9ad7855c70fd0b0a7b4459976",
+    api_key: "d07205f9ad7855c70fd0b0a7b4459976",
     language: "en-US"
   }
 });
