13491df0dad50f32a041b6276b95214c28cc63f2
BEUMGOO <captain_kbg@naver.com>
2019-08-14 22:57:00 +0900

    Updates

---
+++ precache-manifest.fd0c1257840b04f70733e67ebec169e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f94a11a047bb555ad964994547a035",
+    "url": "/gooflix/index.html"
+  },
+  {
+    "revision": "4a2f4b3d828882c99b7e",
+    "url": "/gooflix/static/js/2.d7e6b97f.chunk.js"
+  },
+  {
+    "revision": "7cdc11dcc3b2dc8134c8",
+    "url": "/gooflix/static/js/main.d70d2732.chunk.js"
+  },
+  {
+    "revision": "23b0fe774fad6c05588c",
+    "url": "/gooflix/static/js/runtime~main.4192c076.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/gooflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
